26fbfe04dba3ea9002ee42aa685a322ad4605469
Deep Patel <deepp0925@gmail.com>
2025-04-04 14:04:54 -0400

    some changes

--- src/main.py
+++ src/main.py
@@ -1,20 +1,26 @@
-from typing import Union
-from .logger import logger, log_request
-from .routes import router
-from fastapi import FastAPI, Request
+from .logger import log_request
+# from .routes import router
+from fastapi import FastAPI
 from starlette.middleware.base import BaseHTTPMiddleware
 
 app = FastAPI()
 
 app.add_middleware(BaseHTTPMiddleware, dispatch=log_request)
-app.include_router(router=router)
+# app.include_router(router=router)
 
 
-# from ai.modal import Modal, ModalName
-# from PIL import Image
+from docling.datamodel.base_models import DocumentStream
+from io import BytesIO
+from typing import Annotated, Union
+from fastapi import UploadFile, File, Depends, Request, Form
+from PIL import Image
 
-# img = Image.open("./1.jpg")
-# modal = Modal(ModalName.GEMMA3_4B)
-# res = modal.invoice_info(img)
+@app.post('/ocr/invoice')
+async def tfn(file: Union[UploadFile, None] = None):
 
-# print(res.unwrap().to_json())
+    if not file:
+        return {"message": "No upload file sent"}
+
+    file = await file.read()
+    # img = Image.open(file)
+    return len(file)
--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,7 +2,7 @@
 # - /ocr/doc
 # - /ocr/invoice
 # - /ocr/chargeback
-
+from typing import Annotated
 from fastapi import APIRouter, File, UploadFile, HTTPException, Response
 from docling.document_converter import DocumentConverter
 from docling.datamodel.base_models import DocumentStream
@@ -17,24 +17,24 @@ router = APIRouter()
 docling_converter = DocumentConverter()
 
 @router.post("/ocr/doc")
-async def doc(file: UploadFile):
+async def doc(file: Annotated[bytes, File()]):
     try:
-        file_type = None
-        if file.filename.endswith((".doc", ".docx")):
-            file_type = "Word Document"
-        elif file.filename.endswith(".pdf"):
-            file_type = "PDF"
-        elif file.filename.endswith((".jpg", ".jpeg", ".png", ".tif", ".tiff")):
-            file_type = "Image"
+        # file_type = None
+        # if file.filename.endswith((".doc", ".docx")):
+        #     file_type = "Word Document"
+        # elif file.filename.endswith(".pdf"):
+        #     file_type = "PDF"
+        # elif file.filename.endswith((".jpg", ".jpeg", ".png", ".tif", ".tiff")):
+        #     file_type = "Image"
 
-        if file_type is None:
-            return HTTPException(406, json.dumps({"error": "Can only accept images, documents and pdfs"}))
+        # if file_type is None:
+        #     return HTTPException(406, json.dumps({"error": "Can only accept images, documents and pdfs"}))
         
 
-        content = await file.read()
-        file_content = BytesIO(content)
+        # content = await file.read()
+        file_content = BytesIO(file)
 
-        result = docling_converter.convert(DocumentStream(stream=file_content, name=file.filename))
+        result = docling_converter.convert(DocumentStream(stream=file_content, name="unknowns"))
         dic = result.document.export_to_dict()
 
         return json.dumps(dic)
@@ -49,14 +49,9 @@ ai_modal = Modal(ModalName.GEMMA3_4B)
 
 #  Uses AI modal to retrieve trivial information from the image
 @router.post("/ocr/invoice")
-async def invoice(file: UploadFile):
+async def invoice(file: Annotated[bytes, File()]):
     try:
-        if not file.filename.endswith((".jpg", ".jpeg", ".png", ".tif", ".tiff")):
-            return HTTPException(406, json.dumps({"error": "Can only accept images"}))
-        
-
-        content = await file.read()
-        file_content = BytesIO(content)
+        file_content = BytesIO(file)
         img = Image.open(file_content)
 
         return ai_modal.invoice_info(img).to_json()
