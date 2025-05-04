e9be267af192fc7ec8923e7ae8fe7c4e02b74419
Deep Patel <deepp092@gmail.com>
2025-05-04 12:04:55 -0400

    somemore updates

--- qwen2.py
+++ qwen2.py
@@ -1,10 +1,27 @@
-from transformers import Qwen2_5_VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
 from qwen_vl_utils import process_vision_info
 from PIL import Image
 import json
+import torch
+from platform import system
+
+model_id = "Qwen/Qwen2.5-VL-7B-Instruct"
+
+attn_implementation = "flash_attention_2" if True else "eager"
+
+# runs on the fastest device possible
+def device() -> str:
+    if torch.cuda.is_available():
+        return "cuda"
+    
+    if system() == "Darwin":
+        return "mps"
+    
+    return "cpu"
+
 # default: Load the model on the available device(s)
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2.5-VL-7B-Instruct", torch_dtype="auto", device_map="auto"
+    model_id, torch_dtype="auto", device_map="auto", attn_implementation=attn_implementation
 )
 
 # We recommend enabling flash_attention_2 for better acceleration and memory saving, especially in multi-image and video scenarios.
@@ -16,7 +33,7 @@ model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
 # )
 
 # default processer
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
+processor = AutoProcessor.from_pretrained(model_id)
 
 # The default range for the number of visual tokens per image in the model is 4-16384.
 # You can set min_pixels and max_pixels according to your needs, such as a token range of 256-1280, to balance performance and cost.
@@ -26,28 +43,13 @@ processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
 img =Image.open("./5.png")
 messages = [
     {
-                "role": "system",
-                "content": [{"type": "text", "text": "You are a OCR Engine with advanced reasoning on invoices. Respond each question on separate line"}]
-            },
-    {
         "role": "user",
         "content": [
             {"type": "image", "image": img},
-            {"type": "text", "text": "Who is the vendor?"},
-                    {"type": "text", "text": "What is the invoice number?"},
-                    {"type": "text", "text": "What is the account or customer number?"},
-                    {"type": "text", "text": "What is the date of the invoice in MM/DD/YYYY format?"},
-                    {"type": "text", "text": "What is the due date in MM/DD/YYYY format?"},
-                    {"type": "text", "text": "What is the total amount?"}
+            {"type": "text", "text": "retrieve seller, invoice_date, due_date, bill_to or ship_to address, invoice_number, customer_number or account_number, total. return response in JSON format. "}
         ]
     },
 ]
-
-
-            
-            
-            
-
 
 # Preparation for inference
 text = processor.apply_chat_template(
@@ -61,7 +63,7 @@ inputs = processor(
     padding=True,
     return_tensors="pt",
 )
-inputs = inputs.to("mps")
+inputs = inputs.to(device())
 
 # Inference: Generation of the output
 generated_ids = model.generate(**inputs, max_new_tokens=512)
